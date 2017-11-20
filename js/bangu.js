5452ea7163ad868371d529fdcce11eacb2cbc562
guskant <gusni.kantu@gmail.com>
2017-11-20 12:34:25 +0000

    bangu

--- js/bangu.js
+++ js/bangu.js
@@ -1,9 +1,5 @@
 $('[lang="jbo"]').hide();
 $('[lang="epo"]').hide();
-$('[lang="fr"]').hide();
-$('[lang="zh-Hant"]').hide();
-$('[lang="zh-Hans"]').hide();
-$('[lang="ja"]').hide();
 
 $('#lang-switch').change(function () {
   var lang = $(this).val();
@@ -19,22 +15,6 @@ $('#lang-switch').change(function () {
     case 'epo':
       $('[lang]').hide();
       $('[lang="epo"]').show();
-      break;
-    case 'fr':
-      $('[lang]').hide();
-      $('[lang="fr"]').show();
-      break;
-    case 'zh-Hant':
-      $('[lang]').hide();
-      $('[lang="zh-Hant"]').show();
-      break;
-    case 'zh-Hans':
-      $('[lang]').hide();
-      $('[lang="zh-Hans"]').show();
-      break;
-    case 'ja':
-      $('[lang]').hide();
-      $('[lang="ja"]').show();
       break;
     default:
       $('[lang]').hide();
